bba4f4d4c885e8955b4c979433c021821c4c3370
mskrasnov <linuxoid85@gmail.com>
2025-02-04 19:01:22 +0300

    Add new experimental TUI

--- src/tui_new.rs
+++ src/tui_new.rs
@@ -1,7 +1,7 @@
 //! Experimental TUI for `f`
 
 use crate::consts::{PROG_NAME, PROG_VER};
-use crate::tui::colors::{color_from_u8, get_style, Colors, Modifier, Panels};
+use crate::tui::colors::{color_from_u8, get_style, Colors, FileColor};
 use crate::utils::get_home;
 use crate::{traits::Toml, utils::read_dir, FileEntry, FileType};
 
@@ -22,131 +22,6 @@ use std::{
     path::{Path, PathBuf},
     str::FromStr,
 };
-
-/// Get style for list_files panel
-///
-/// ## Usage
-///
-/// ```
-/// let entry = FileEntry { ... };
-/// let fcol = FileColor {
-///     entry: &entry,
-///     cols: Panels::default(),
-///     selected: true,
-/// };
-/// let color = fcol.style();
-/// ```
-#[derive(Clone)]
-struct FileColor<'a> {
-    entry: &'a FileEntry,
-    cols: Panels,
-    selected: bool,
-}
-
-impl<'a> FileColor<'a> {
-    fn get_bg(&self) -> Option<Color> {
-        color_from_u8(self.cols.selection_color)
-    }
-
-    fn get_fg_hidden_u8(&self, c: u8) -> u8 {
-        if c <= 38 {
-            c + 60
-        } else {
-            c - 60
-        }
-    }
-
-    fn get_fg_unselected(&self) -> Option<Color> {
-        if self.entry.is_hidden {
-            match self.entry.file_type {
-                FileType::Directory | FileType::ParentDirectory => color_from_u8(self.cols.dir),
-                FileType::Link => color_from_u8(self.cols.link),
-                FileType::Special => color_from_u8(self.cols.special_file),
-                _ => color_from_u8(self.cols.file),
-            }
-        } else {
-            /* Для скрытого файла нужно использовать в зависимости от основного цвета:
-             *  - более светлый цвет. Например, если для обычного файла был Blue, для
-             *    скрытого - Light Blue;
-             *  - более тёмный цвет. Например, Light Blue -> Blue;
-             *
-             * Для этого смотрим значение основного цвета и прибавляем/вычитаем из
-             * него 60, поскольку разница между обычным и светлым вариантом - 60.
-             */
-            color_from_u8(match self.entry.file_type {
-                FileType::Directory | FileType::ParentDirectory => {
-                    self.get_fg_hidden_u8(self.cols.dir)
-                }
-                FileType::Link => self.get_fg_hidden_u8(self.cols.link),
-                FileType::Special => self.get_fg_hidden_u8(self.cols.special_file),
-                _ => self.get_fg_hidden_u8(self.cols.file),
-            })
-        }
-    }
-
-    fn get_unselected_u8(&self, c1: Option<u8>, c2: u8) -> u8 {
-        match c1 {
-            Some(color_unselected) => color_unselected,
-            None => c2,
-        }
-    }
-
-    fn get_fg_selected(&self) -> Option<Color> {
-        /* Для упрощения кода для скрытых файлов будет использоваться тот же цвет
-         * выделения, что и для не-скрытого. */
-        match self.entry.file_type {
-            FileType::Directory | FileType::ParentDirectory => {
-                color_from_u8(self.get_unselected_u8(self.cols.dir_selected, self.cols.dir))
-            }
-            FileType::Link => {
-                color_from_u8(self.get_unselected_u8(self.cols.link_selected, self.cols.link))
-            }
-            FileType::Special => color_from_u8(
-                self.get_unselected_u8(self.cols.special_file_selected, self.cols.special_file),
-            ),
-            _ => color_from_u8(self.get_unselected_u8(self.cols.file_selected, self.cols.file)),
-        }
-    }
-
-    fn get_modifier(&self) -> Modifier {
-        Modifier::from(match self.entry.file_type {
-            FileType::File | FileType::FileExecutable => self.cols.file_modifier.unwrap_or(8),
-            FileType::Directory | FileType::ParentDirectory => self.cols.dir_modifier.unwrap_or(8),
-            FileType::Link => self.cols.link_modifier.unwrap_or(8),
-            FileType::Special => self.cols.special_file_modifier.unwrap_or(8),
-        })
-    }
-
-    pub fn style(&self) -> Style {
-        let mut style = Style::default()
-            .bg(if self.selected {
-                self.get_bg()
-                    .unwrap_or(color_from_u8(self.cols.background).unwrap_or_default())
-            } else {
-                color_from_u8(self.cols.background).unwrap_or_default()
-            })
-            .fg(if self.selected {
-                self.get_fg_selected().unwrap()
-                // self.get_bg().unwrap()
-            } else {
-                self.get_fg_unselected().unwrap_or_default()
-            });
-
-        let modifier = self.get_modifier();
-        match modifier {
-            Modifier::Bold => style = style.bold(),
-            Modifier::Italic => style = style.italic(),
-            Modifier::Underline => style = style.underlined(),
-            Modifier::BoldItalic => style = style.bold().italic(),
-            Modifier::BoldUnderline => style = style.bold().underlined(),
-            Modifier::ItalicUnderline => style = style.italic().underlined(),
-            Modifier::BoldItalicUnderline => style = style.bold().italic().underlined(),
-            _ => {}
-        }
-
-        style
-    }
-}
 
 /// Main `f` TUI
 pub struct F {
@@ -224,11 +99,17 @@ impl F {
         match key_event.code {
             KeyCode::F(8) => {
                 if let Some(selected) = &self.selected {
-                    if let Err(why) = selected.remove() {
-                        self.error_text = Some(why.to_string());
+                    if selected.file_name == OsString::from_str(".. [UP]").unwrap()
+                        || self.idx == Some(0)
+                    {
+                        self.error_text = Some("Failed to remove parent directory".to_string());
                     } else {
-                        if let Err(why) = self.rescan_dir() {
+                        if let Err(why) = selected.remove_bin() {
                             self.error_text = Some(why.to_string());
+                        } else {
+                            if let Err(why) = self.rescan_dir() {
+                                self.error_text = Some(why.to_string());
+                            }
                         }
                     }
                 }
@@ -313,18 +194,25 @@ impl F {
             }
 
             KeyCode::Esc => self.remove_error_msg(),
+            KeyCode::Char('c') => self.update_colors(),
             _ => {}
+        }
+    }
+
+    fn update_colors(&mut self) {
+        // if let Ok(cols) = Colors::parse("./colors.toml") {
+        //     self.colors = cols;
+        // }
+        match Colors::parse("./colors.toml") {
+            Ok(colors) => self.colors = colors,
+            Err(why) => self.error_text = Some(why.to_string()),
         }
     }
 
     fn ui(&mut self, frame: &mut Frame) {
         let chunks = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(1),
-                Constraint::Min(5),
-                Constraint::Length(1),
-            ])
+            .constraints([Constraint::Min(5), Constraint::Length(1)])
             .split(frame.area());
 
         let tcols = self.colors.title;
@@ -339,17 +227,17 @@ impl F {
                 .fg(Color::Red)
                 .centered(),
         };
-        frame.render_widget(title, chunks[0]);
+        frame.render_widget(title, chunks[1]);
 
         let fcols = self.colors.panels;
         let mut files_block = Block::bordered()
             .border_set(border::DOUBLE)
             .style(
                 Style::default()
-                    .bg(color_from_u8(fcols.background).unwrap_or_default())
+                    // .bg(color_from_u8(fcols.border_active).unwrap_or_default())
                     .fg(color_from_u8(fcols.file).unwrap_or_default()),
             )
-            .set_style(get_style(fcols.background, fcols.file_modifier))
+            .set_style(get_style(fcols.border_active, fcols.file_modifier))
             .title(match &self.selected {
                 Some(selected) => format!(
                     " {} ({}/{}) ",
@@ -360,10 +248,7 @@ impl F {
                 None => String::new(),
             })
             .title_top(
-                Line::from(format!(" {} files in this dir ", self.rows.len()))
-                    .right_aligned()
-                    // .bg(Color::Gray)
-                    // .fg(Color::Black),
+                Line::from(format!(" {} files in this dir ", self.rows.len())).right_aligned(),
             )
             .title_top(
                 Line::from(format!(
@@ -371,8 +256,8 @@ impl F {
                     fs::canonicalize(&self.current_dir).unwrap().display()
                 ))
                 .centered()
-                .bg(Color::Gray)
-                .fg(Color::Black),
+                .bg(color_from_u8(fcols.header_bg).unwrap_or_default())
+                .fg(color_from_u8(fcols.header_fg).unwrap_or_default()),
             );
 
         if self.show_hidden {
@@ -418,7 +303,10 @@ impl F {
         ];
 
         let table = Table::new(rows, widths)
-            .row_highlight_style(Style::new().bg(Color::Cyan))
+            .style(Style::new().bg(color_from_u8(fcols.background).unwrap_or_default()))
+            .row_highlight_style(
+                Style::new().bg(color_from_u8(fcols.selection_color).unwrap_or_default()),
+            )
             .block(files_block.clone());
 
         StatefulWidget::render(
